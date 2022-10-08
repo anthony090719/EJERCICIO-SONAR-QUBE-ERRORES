7a10317b8f864eac838d31841efcefdbbe516199
anthony090719 <50929169+anthony090719@users.noreply.github.com>
2022-10-07 19:43:46 -0600

    Add files via upload

---
+++ codigo-incorrecto.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Ejemplo-incorrecto</title>
+    </head>
+    <body>
+
+        <?php
+            echo Hola, soy un script de PHP!";
+            
+        ?>
+
+    </body>
+</html>
